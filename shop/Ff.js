b0539cf5cafbee1eab8f8bdd3915fc064274961b
helmaa <96760196+helmaa@users.noreply.github.com>
2021-12-29 20:35:13 +0700

    Ff.js
    .

---
+++ shop/Ff.js
@@ -0,0 +1,31 @@
+const list_ff = (prefix) => {
+return `*HELIA STORE*
+				
+PROSES	1 - 10Mnt			
+OPEN	08.00-22.00			
+FORMAT	ID			
+				
+LIST DIAMOND FREE FIRE
+
+ᵎᵎ 🌻 ʾ 5 💎 816
+ᵎᵎ 🌻 ʾ 20 💎 2.720
+ᵎᵎ 🌻 ʾ 50 💎 6.528
+ᵎᵎ 🌻 ʾ 70 💎 8.976
+ᵎᵎ 🌻 ʾ 100 💎 13.056
+ᵎᵎ 🌻 ʾ 140 💎 17.952
+ᵎᵎ 🌻 ʾ 210 💎 26.928
+ᵎᵎ 🌻 ʾ 355 💎 44.880
+ᵎᵎ 🌻 ʾ 500 💎 63.650
+ᵎᵎ 🌻 ʾ 720 💎 89.760
+ᵎᵎ 🌻 ʾ 1000 💎 124.750
+ᵎᵎ 🌻 ʾ 1440 💎 178.300
+ᵎᵎ 🌻 ʾ 2000 💎 244.900
+ᵎᵎ 🌻 ʾ MM :27.200
+ᵎᵎ 🌻 ʾ MB :136.000
+
+Untuk pay / pembayan silahkan ketik ${prefix}pay
+`
+	}
+
+exports.list_ff = list_ff
+ 
